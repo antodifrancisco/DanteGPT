271f5a5933f2f5e0697ecd6c282a1bb6ccd44f5c
Antonio Di Francesco <adifrancesco@tenstorrent.com>
2024-08-12 22:47:19 -0600

    Model recreated like HF

--- GPT2/train_gpt2.py
+++ GPT2/train_gpt2.py
@@ -90,6 +90,23 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+    def forward(self, idx):
+        # idx is of shape (B, T)
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size"
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+        pos_emb = self.transformer.wpe(pos)
+        tok_emb = self.transformer.wte(idx)
+        x = tok_emb + pos_emb
+
+        for block in self.transformer.h:
+            x = block(x)
+        
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x)
+        return logits
+
+
     @classmethod
     def from_pretrained(cls, model_type: GPT2ModelType):
         """Loads pretrained GPT-2 model weights from huggingface"""
@@ -146,6 +163,35 @@ class GPT(nn.Module):
 
 # ----------------------------------------------
 
-model_type = GPT2ModelType.GPT2
-model = GPT.from_pretrained(model_type)
-print("Didn't crash yet")
+num_return_sequences = 5
+max_length = 30
+
+gpt2 = GPT2ModelType.GPT2
+model = GPT.from_pretrained(gpt2)
+model.eval()
+# model.to("cuda")
+
+import tiktoken
+enc = tiktoken.get_encoding("gpt2")
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens
+# x = tokens.to("cuda")
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+    with torch.no_grad():
+        logits = model(x)
+        logits = logits[:,-1,:]
+        probs = F.softmax(logits,dim=-1)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        ix = torch.multinomial(topk_probs, 1)
+        xcol = torch.gather(topk_indices, -1, ix)
+        x = torch.cat((x,xcol), dim=1)
+
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
